7667a9f155976890a4340c68ebf36411c8078859
Arnab Bishakh Sarker <bishakh99@gmail.com>
2024-08-02 05:15:42 +0600

    dark mode beta

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+function toggleDarkMode() {
+  const elements = document.querySelectorAll('body, a, nav, .nav-links, .logo, #hamburger-nav, .hamburger-icon span, .menu-links, section, #profile .section__text p, #profile .title, .about-containers, .about-details-container, .details-container, .arrow, .btn, .Skills-sub-title, .details-container.color-container, .contact-info-container');
+  elements.forEach(element => {
+    element.classList.toggle('dark-mode');
+  });
+}
+
+// Add event listener for a dark mode toggle button
+document.querySelector('#dark-mode-toggle').addEventListener('click', toggleDarkMode);
+
