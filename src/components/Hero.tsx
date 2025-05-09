8e368724fc83d9c9c353bf9fb4bf562a2dd2df93
arnabishakh <bishakh99@gmail.com>
2025-05-10 04:16:07 +0600

    dp error

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -33,7 +33,7 @@ const Hero = () => {
           <div className="w-full md:w-1/2 flex justify-center md:justify-end animation-delay-300 animate-fade-in">
             <div className="w-64 h-64 md:w-80 md:h-80 rounded-full overflow-hidden border-8 border-white shadow-xl">
               <img 
-                src="public\bishakh.png"
+                src="**\public\bishakh.png"
                 alt="Arnab Bishakh Sarker" 
                 className="w-full h-full object-cover"
               />
